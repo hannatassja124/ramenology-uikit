7452846ddfe8bdfb4cf99fdf8bb2e630636c2b65
maritia <maritia.pangaribuan@gmail.com>
2021-06-16 08:42:09 +0700

    Feat: Ramen Detail Page

---
+++ Ramenology-uikit/Views/EmptyStateTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EmptyStateTableViewCell.swift
+//  Ramenology-uikit
+//
+//  Created by Maritia Pangaribuan on 15/06/21.
+//
+
+import UIKit
+
+class EmptyStateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
